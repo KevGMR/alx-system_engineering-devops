f516a222326c3dfcd6980cbc6ca8a15773fdc44d
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-15 22:35:38 +0300

    3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,32 @@ import requests
 
 def number_of_subscribers(subreddit):
 
-    # url = "https://www.reddit.com/r/{:s}/about.json".format(subreddit)
-    # headers = {'User-Agent': 'MyBot/0.0.1'}
+    # # url = "https://www.reddit.com/r/{:s}/about.json".format(subreddit)
+    # # headers = {'User-Agent': 'MyBot/0.0.1'}
 
-    req = requests.get(
-        "https://www.reddit.com/r/{:s}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"}, timeout=10, allow_redirects=False
-    )
+    # req = requests.get(
+    #     "https://www.reddit.com/r/{:s}/about.json".format(subreddit),
+    #     headers={"User-Agent": "Custom"}, timeout=10, allow_redirects=False
+    # )
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    return 0
+    # if req.status_code == 200:
+    #     return req.json().get("data").get("subscribers")
+    # return 0
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = "https://www.reddit.com/r/{:s}/about.json".format(subreddit)
+
+    try:
+        # Make a GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            # Return the number of subscribers
+            return data['data']['subscribers']
+        else:
+            # If the subreddit is invalid or other errors occur, return 0
+            return 0
+    except requests.RequestException:
+        # In case of network errors or other request issues, return 0
+        return 0
